236a2ae6e9159ab725ba4d5f636d838b7530103a
Laura Hurtado <laura.hurtadoh@cun.edu.co>
2020-02-17 16:35:26 -0500

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,51 @@ export class AppComponent {
 
   // array
   items = [ 'laura', 'maria', 'nicolas'];
+// array de objetos 
+  products: IProduct[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
   // metodo agregar 
   additems() {
     this.items.push ('nuevo item');
