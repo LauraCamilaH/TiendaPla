919fc93259ad8362576bc2dea993f5e5cdca7bbd
Laura Hurtado <laura.hurtadoh@cun.edu.co>
2020-02-18 16:30:55 -0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';}
+import {ProductComponent} from './componets/product.componets';
 import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
